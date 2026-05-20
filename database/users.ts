4ef0788963fd31c7e9557d2fdf274beb8acf162d
AbdalrhmanTamimi <abdalrahmantamimi09@gmail.com>
2026-05-20 19:58:40 +0300

    Update users.ts

--- database/users.ts
+++ database/users.ts
@@ -10,10 +10,7 @@ const schema = new mongoose.Schema({
     },password:{
         type:String,
         required:true,
-    },email:{
-        type:String,
-        minlength : 5,
-        unique:true
+    
     }
 
 })
